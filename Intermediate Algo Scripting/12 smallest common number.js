2f296424a5e6d4752e777fcc5f58777a4fb09bb7
Jithu R Jacob <jithurjacob@gmail.com>
2017-06-27 20:31:50 +0530

    finished smallest common number

--- Intermediate Algo Scripting/12 smallest common number.js
+++ Intermediate Algo Scripting/12 smallest common number.js
@@ -1,7 +1,34 @@
-
-function smallestCommons(arr) {
-  return arr;
+function gcd(a, b){
+    // Euclidean algorithm
+    var t;
+    while (b != 0){
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
 }
 
+function lcm(a, b){
+    return (a * b / gcd(a, b));
+}
 
-smallestCommons([1,5]);
+function smallestCommons(arr) {
+    var min = arr.reduce(function(min_,elem){
+        return min_>elem?elem:min_;
+    });
+    var max = arr.reduce(function(max_,elem){
+        return max_<elem?elem:max_;
+    });
+    if(DEBUG)
+        console.log("min=",min," max=",max);
+    var num =1;
+    for(var i=min;i<=max;i++){console.log(i);
+        num = lcm(num,i);
+    }
+  return num;
+}
+var DEBUG = true;
+
+console.log(smallestCommons([1,5]));
+// console.log(lcm(2,3));
