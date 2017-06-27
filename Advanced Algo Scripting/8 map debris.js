5f4e7385d113b328d448822e122cb3c853aa8543
Jithu R Jacob <jithurjacob@gmail.com>
2017-06-27 18:27:51 +0530

    working on map debris

---
+++ Advanced Algo Scripting/8 map debris.js
@@ -0,0 +1,27 @@
+
+function orbitalPeriod(arr) {
+    var GM = 398600.5;
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    //   var earthRadius = 6378.14;
+    var arr_ = [];
+    arr.forEach(function (element) {
+        var temp = {};
+        temp["name"] = element["name"];
+        var PI = (22 / 7);
+        var v = Math.round(Math.pow((GM / (earthRadius + element["avgAlt"])), 0.5));
+        console.log(v);
+        var p = 2 * PI * ((earthRadius + element["avgAlt"]) / v);
+        temp["orbitalPeriod"] = p;
+        arr_.push(temp);
+    }, this);
+
+    return arr_;
+}
+
+console.log(orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]));
+//86400
+console.log(orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]));
+//[{name : "iss", orbitalPeriod: 5557}, 
+//{name: "hubble", orbitalPeriod: 5734}, 
+//{name: "moon", orbitalPeriod: 2377399}].
